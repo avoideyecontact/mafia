a74b710172a00d00ac4e1544b612b04de40c2ac1
deus <vendetta_gt@mail.ru>
2023-07-05 18:53:21 +0500

    Наши семьи блок

--- front/when-the-city-falls-asleep/src/Pages/MainPage.js
+++ front/when-the-city-falls-asleep/src/Pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainSubheader from "../Modules/MainSubheader";
 import CounterModule from "../Modules/CounterModule";
 import BloodTrailSection from "../Modules/BloodTrailSection";
+import OurFamilies from '../Modules/OurFamilies';
 
 const MainPage = () => {
     return (
@@ -9,6 +10,7 @@ const MainPage = () => {
             <MainSubheader/>
             <CounterModule/>
             <BloodTrailSection/>
+            <OurFamilies/>
         </>
     );
 };
